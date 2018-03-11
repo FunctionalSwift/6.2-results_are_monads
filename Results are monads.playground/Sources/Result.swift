171afdda2eed6863c3d5a3ae1e4771ad6f632c74
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-11 19:24:33 +0100

    FlatMapping our Map!!

--- Results are monads.playground/Sources/Result.swift
+++ Results are monads.playground/Sources/Result.swift
@@ -2,13 +2,8 @@ public enum Result<A, E> {
     case success(_: A)
     case failure(_: E)
     
-    public func map<B>(_ transform: (A) -> B) -> Result<B, E> {
-        switch self {
-        case let .success(value):
-            return .success(transform(value))
-        case let .failure(reason):
-            return .failure(reason)
-        }
+    func map<B>(_ transform: (A) -> B) -> Result<B, E> {
+        return self.flatMap { .success(transform($0)) }
     }
 }
 
